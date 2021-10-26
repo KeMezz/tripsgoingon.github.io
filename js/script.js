37085e0616e2a0dd1b670f16bfdb99cb17ba9a45
KeMezz <pnescene@icloud.com>
2021-10-26 17:43:20 +0900

    마무리

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 const colors = ["#E5F5F5", "#DBD8F6", "#F4DBEB", "#F1F8DC"];
 
+let count = 0;
+
+if (count > 4) {
+    count = 0;
+}
+
 $('.right').click(function() {
+    count = count + 1;
+    if (count > 3) {
+        count = count - 1;
+    }
     $('.secB___container.active').next().addClass("active");
     $('.secB___container.active').prev().removeClass("active");
     $('.secB___page>div.active').next().addClass("active");
     $('.secB___page>div.active').prev().removeClass("active");
+    $('.secB').css('backgroundColor',colors[count]);
 })
 
 $('.left').click(function() {
+    count = count - 1;
+    if (count < 0) {
+        count = count + 1;
+    }
     $('.secB___container.active').prev().addClass("active");
     $('.secB___container.active').next().removeClass("active");
     $('.secB___page>div.active').prev().addClass("active");
     $('.secB___page>div.active').next().removeClass("active");
+    $('.secB').css('backgroundColor',colors[count]);
 })
 
 $('.xi-bars').click(function() {
